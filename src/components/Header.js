e497d6920d2ca601bffa6db0c7a06da092428526
tabathambell <bell.t.8@pg.com>
2024-09-17 12:26:38 -0400

    setting up repo

---
+++ src/components/Header.js
@@ -0,0 +1,32 @@
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import { Space_Grotesk } from "next/font/google";
+import Image from "next/image";
+
+const Space = Space_Grotesk({ subsets: ["latin"] });
+
+export default function Header() {
+  return (
+    <Box
+      component="section"
+      sx={{ p: 3, borderBottom: "3px solid black", backgroundColor: "#4c3269", width: '100%', display: 'flex', alignItems: "center", flexDirection: 'row', height: '100px' }}
+    >
+      <Box sx={{ width: '15%' }}>
+        <Typography
+          className={Space.className}
+          sx={{ color: "white", fontSize: 28 }}
+        >
+          TABATHA BELL
+        </Typography>
+      </Box>
+      <Box sx={{ width: '40%', pt: 1 }}>
+        <Image
+          src="/hearts.gif"
+          width="125"
+          height="100"
+          alt="Chandelure from Pokemon"
+        />
+      </Box>
+    </Box>
+  );
+}
